e264e7f3b2e12a3c74aba528679f0f8bab32a865
Or Amit <oramit88@gmail.com>
2016-05-04 21:45:54 +0300

    final- v15
    final- v15

--- Componet/Source.cpp
+++ Componet/Source.cpp
@@ -8,7 +8,9 @@
 #include "ComboBox.h"
 using namespace std;
 
-
+/*
+Uncoment cases to test the Components!!
+*/
 int main()
 {
 	vector <string> data;
@@ -16,30 +18,59 @@ int main()
 	data.push_back("option B");
 	data.push_back("option C");
 	data.push_back("option D");
-	
-	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	// *component = new Label(COORD{ 3,2 }, 0, 0, h, "hellew nice to meet you go home..");
-	//component->draw();
-	
-	//Component *component2 = new Label(COORD{ 10,10 }, 0, 0, h, "hellew nice to meet you go home..");
-	//component2->draw();
 	ComponentObserver observer;
-	DWORD textColor = FOREGROUND_GREEN;
-	//DWORD wAttr2 = BACKGROUND_BLUE;
-	Component *comp3 = new CheckList(COORD{20,20 }, textColor, BACKGROUND_BLUE, h, data, 10, 20);
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	DWORD backgroudBlue = BACKGROUND_BLUE;
+	DWORD backgroudBlack = 0;
+	DWORD backgrounRed = BACKGROUND_RED;
+	DWORD textColor = FOREGROUND_RED;
+	DWORD foregroundGreen = FOREGROUND_GREEN;
+	DWORD foregrounBlue = FOREGROUND_BLUE;
 
-	//Component *comp3 = new ComboBox(COORD{ 2,3 }, textColor, BACKGROUND_BLUE,h, "hello", data);
-	//ComboBox(COORD coord, DWORD backgroundColor, DWORD textColor, HANDLE handle, string text, vector <string> data)
-	//
-	//Component *comp3 = new TextBox(COORD{ 2,3 }, textColor, BACKGROUND_RED, h, 20);
-//	Component *comp4 = new RadioList(COORD{ 15,15 }, textColor, BACKGROUND_BLUE, h, data, 10, 20);
+
+	/*
+	Uncoment cases to test the Components!!
+	*/
+
+	Component *chekList = new CheckList(COORD{ 5,5 }, textColor, 0, h, data, 6, 14);
+	chekList->draw();
+	observer.addObserver(chekList);
 	
-	comp3->draw();
-	//comp3->hide();
-	//comp4->draw();
-	observer.addObserver(comp3);
-//	observer.addObserver(comp4);
+	
+	
+	// case label
+	/*
+	Component *label = new Label(COORD{ 5,5 }, 0, foregroundGreen, h, "Hello world!");
+	label->draw();
+	observer.addObserver(label);
+	*/
+
+	// case textBoc
+	/*
+	Component *textBox = new TextBox(COORD{ 5,5 }, backgrounRed, FOREGROUND_BLUE, h, 20);
+	textBox->draw();
+	observer.addObserver(textBox);
+	*/
+
+	
+
+	//case RadioList
+	/*
+	Component *radioList = new RadioList(COORD{ 5,5 }, textColor, 0, h, data, 6, 14);
+	radioList->draw();
+	observer.addObserver(radioList);
+	*/
+
+	//case  ComboBox
+	//press on the combo box in order to see the options list.
+	//after you choose the option' press on the comboBox again
+	/*
+	Component *comboBox = new ComboBox(COORD{ 5,5 }, textColor, 0,h, "hello", data);
+	comboBox->draw();
+	observer.addObserver(comboBox);
+	*/
 
 	observer.listen();
+
+
 }
